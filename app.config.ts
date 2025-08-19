1c1fec29391c5f6a53c95e73eeeeae8bb711fab9
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-08-19 12:49:12 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,33 +1,54 @@
 // app.config.ts
-//...
-export default {
-  //...
-  ios: {
-    bundleIdentifier: 'com.superfanverified',
-    supportsTablet: true
-  },
-  android: {
-    package: 'com.superfanverified',
-    adaptiveIcon: {
-      foregroundImage: './assets/adaptive-icon.png',
-      backgroundColor: '#ffffff'
-    }
-  },
-  extra: {
-    // Expose secrets from .env
-    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
-  },
-  plugins: [
-    [
-      "expo-build-properties",
-      {
-        "android": {
-          "minSdkVersion": 25
-        }
-      }
+module.exports = {
+  expo: {
+    name: "SuperfanVerified",
+    slug: "superfanverified",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: [
+      "**/*"
     ],
-    "@logrocket/react-native"
-  ]
+    ios: {
+      bundleIdentifier: "com.oracle69digitalmarketing.superfanverified",
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      package: "com.oracle69digitalmarketing.superfanverified"
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    extra: {
+      eas: {
+        projectId: "8203d4d6-e559-4279-9ddc-4403c4243c9f"
+      },
+      // You should not hardcode these values
+      RPC_URL: "https://rpc.xion-testnet-2.burnt.com:443",
+      CHAIN_ID: "xion-testnet-2",
+      spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+      spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    },
+    plugins: [
+      [
+        "expo-build-properties",
+        {
+          "android": {
+            "minSdkVersion": 25
+          }
+        }
+      ],
+      "@logrocket/react-native"
+    ]
+  }
 };
-
