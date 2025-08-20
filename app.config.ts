096f80930efa6640cf89df8be7a7b93004456eaf
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-08-20 18:07:49 +0000

    Resolve merge conflict in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -38,6 +38,9 @@ export default ({ config }: ConfigContext): ExpoConfig => {
                                                                                                                                                               ...(expoConfig.web || {}),
                                                                                                                                                                     favicon: "./assets/favicon.png"
                                                                                                                                                                         },
+                                                                                                                                                                      cli: {
+  appVersionSource: "version"
+},  
                                                                                                                                                                             extra: {
                                                                                                                                                                                   ...extraConfig,
                                                                                                                                                                                         eas: {
