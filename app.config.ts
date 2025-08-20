3684b768b965945783b6c0df7666b957d7c716e5
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-08-20 17:55:21 +0000

    Resolve merge conflict in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,122 +1,66 @@
 // app.config.ts
+import { ExpoConfig, ConfigContext } from '@expo/config';
 
-module.exports = ({ config }) => {
+export default ({ config }: ConfigContext): ExpoConfig => {
+  const expoConfig = config || {};
+    const extraConfig = expoConfig.extra || {};
 
-  // `config` can be empty when running `npx expo config` in a clean environment.
-
-  // We need to provide default empty objects to avoid `undefined` errors.
-
-  const expoConfig = config?.expo || {};
-
-  const extraConfig = expoConfig?.extra || {};
-
-
-  return {
-
-    expo: {
-
-      ...expoConfig,
-
-      name: "SuperfanVerified",
-
-      slug: "superfanverified",
-
-      version: "1.0.0",
-
-      orientation: "portrait",
-
-      icon: "./assets/icon.png",
-
-      userInterfaceStyle: "light",
-
-      splash: {
-
-        image: "./assets/splash.png",
-
-        resizeMode: "contain",
-
-        backgroundColor: "#ffffff"
-
-      },
-
-      assetBundlePatterns: [
-
-        "**/*"
-
-      ],
-
-      ios: {
-
-        bundleIdentifier: "com.oracle69digitalmarketing.superfanverified",
-
-        supportsTablet: true
-
-      },
-
-      android: {
-
-        adaptiveIcon: {
-
-          foregroundImage: "./assets/adaptive-icon.png",
-
-          backgroundColor: "#ffffff"
-
-        },
-
-        package: "com.oracle69digitalmarketing.superfanverified"
-
-      },
-
-      web: {
-
-        favicon: "./assets/favicon.png"
-
-      },
-
-      extra: {
-
-        ...extraConfig,
-
-        eas: {
-
-          projectId: "8203d4d6-e559-4279-9ddc-4403c4243c9f"
-
-        },
-
-        RPC_URL: "https://rpc.xion-testnet-2.burnt.com:443",
-
-        CHAIN_ID: "xion-testnet-2",
-
-        spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-
-        spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
-
-      },
-
-      plugins: [
-
-        [
-
-          "expo-build-properties",
-
-          {
-
-            "android": {
-
-              "minSdkVersion": 25
-
-            }
-
-          }
-
-        ],
-
-        "@logrocket/react-native"
-
-      ]
-
-    }
-
-  };
-
-};
+      return {
+          ...expoConfig,
+              name: "SuperfanVerified",
+                  slug: "superfanverified",
+                      version: "1.0.0",
+                          orientation: "portrait",
+                              icon: "./assets/icon.png",
+                                  userInterfaceStyle: "light",
+                                      splash: {
+                                            image: "./assets/splash.png",
+                                                  resizeMode: "contain",
+                                                        backgroundColor: "#ffffff"
+                                                            },
+                                                                assetBundlePatterns: [
+                                                                      "**/*"
+                                                                          ],
+                                                                              ios: {
+                                                                                    ...(expoConfig.ios || {}),
+                                                                                          bundleIdentifier: "com.oracle69digitalmarketing.superfanverified",
+                                                                                                supportsTablet: true
+                                                                                                    },
+                                                                                                        android: {
+                                                                                                              ...(expoConfig.android || {}),
+                                                                                                                    adaptiveIcon: {
+                                                                                                                            foregroundImage: "./assets/adaptive-icon.png",
+                                                                                                                                    backgroundColor: "#ffffff"
+                                                                                                                                          },
+                                                                                                                                                package: "com.oracle69digitalmarketing.superfanverified"
+                                                                                                                                                    },
+                                                                                                                                                        web: {
+                                                                                                                                                              ...(expoConfig.web || {}),
+                                                                                                                                                                    favicon: "./assets/favicon.png"
+                                                                                                                                                                        },
+                                                                                                                                                                            extra: {
+                                                                                                                                                                                  ...extraConfig,
+                                                                                                                                                                                        eas: {
+                                                                                                                                                                                                ...(extraConfig.eas || {}),
+                                                                                                                                                                                                        projectId: "8203d4d6-e559-4279-9ddc-4403c4243c9f"
+                                                                                                                                                                                                              },
+                                                                                                                                                                                                                    RPC_URL: "https://rpc.xion-testnet-2.burnt.com:443",
+                                                                                                                                                                                                                          CHAIN_ID: "xion-testnet-2",
+                                                                                                                                                                                                                                spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+                                                                                                                                                                                                                                      spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
+                                                                                                                                                                                                                                          },
+                                                                                                                                                                                                                                              plugins: [
+                                                                                                                                                                                                                                                    ...(expoConfig.plugins || []),
+                                                                                                                                                                                                                                                          [
+                                                                                                                                                                                                                                                                  "expo-build-properties",
+                                                                                                                                                                                                                                                                          {
+                                                                                                                                                                                                                                                                                    "android": {
+                                                                                                                                                                                                                                                                                                "minSdkVersion": 25
+                                                                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                                                                                 }
+                                                                                                                                                                                                                                                                                                                       ],
+                                                                                                                                                                                                                                                                                                                             "@logrocket/react-native"
+                                                                                                                                                                                                                                                                                                                                 ]
+                                                                                                                                                                                                                                                                                                                                   };
+                                                                                                                                                                                                                                                                                                                                   };
+                                                                                                                                                                                                                                                                                                                                    
