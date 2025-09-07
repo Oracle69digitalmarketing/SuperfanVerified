e379699b8a97624614c319f5b8031eb3cd0302d9
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-09-08 00:57:43 +0100

    Update babel.config.js6

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,43 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // 🌀 Reanimated must be first
       'react-native-reanimated/plugin',
-      ["module:react-native-dotenv", {
-        "moduleName": "@env",
-        "path": ".env",
-        "safe": false,
-        "allowUndefined": true
-      }]
+
+      // 🔐 Dotenv for environment variables
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+
+      // 📁 Absolute imports (e.g., @components/Button)
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@utils': './src/utils',
+            '@assets': './assets',
+          },
+        },
+      ],
+
+      // 🧠 MobX decorators (if you're using MobX)
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+
+      // 💅 Styled-components (optional but useful)
+      ['babel-plugin-styled-components', { displayName: true }],
     ],
   };
 };
