1d6bd1e2991c2e683d8ae49d769b9b2429de278d
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-09-08 00:42:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+6import React, { useEffect } from 'react';
 import { Text } from 'react-native';
 import { registerRootComponent } from 'expo';
 import * as SplashScreen from 'expo-splash-screen';
