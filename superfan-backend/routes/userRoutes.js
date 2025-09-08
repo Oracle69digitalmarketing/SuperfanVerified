6f95303205309ac793fc453fc3dc9e87b95153ea
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-09-08 01:36:16 +0100

    Update userRoutes.js

--- superfan-backend/routes/userRoutes.js
+++ superfan-backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const { createUser, listUsers } = require('../controllers/userController');
+const {
+  createUser,
+  listUsers
+} = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/users', createUser);
-router.get('/users', listUsers);
+// 🧑 Create or update a user with referral support
+router.post('/', createUser);
+
+// 📋 List recent users with referral metadata
+router.get('/', listUsers);
 
 module.exports = router;
