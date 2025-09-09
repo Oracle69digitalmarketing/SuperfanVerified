01d4facdcf3397d2e1f935370a50a8ac0c760591
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-09-09 16:26:53 +0100

    Update testUsers.js

--- superfan-backend/testUsers.js
+++ superfan-backend/testUsers.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 const BASE = "https://superfan-backend.onrender.com";
 
 async function testUsers() {
-  // Create user
+  console.log("\n=== USERS ===");
   const createRes = await fetch(`${BASE}/users`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -15,9 +15,8 @@ async function testUsers() {
   });
   console.log("Create User:", await createRes.json());
 
-  // List users
   const listRes = await fetch(`${BASE}/users`);
   console.log("List Users:", await listRes.json());
 }
 
-testUsers();
+testUsers().catch(console.error);
