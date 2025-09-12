a7e27ada50d81375ab70be5401a9460ece5a3fe0
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-12 12:27:09 +0100

    Update passport.js
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-backend/config/passport.js
+++ superfan-backend/config/passport.js
@@ -3,75 +3,79 @@ import SpotifyStrategy from "passport-spotify";
 import GoogleStrategy from "passport-google-oauth20";
 import FacebookStrategy from "passport-facebook";
 import TwitterStrategy from "passport-twitter";
+import User from "../models/User.js";
 
-// ===== SPOTIFY =====
-passport.use(
-  new SpotifyStrategy(
-    {
-      clientID: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      callbackURL: process.env.SPOTIFY_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, expires_in, profile, done) => {
-      const user = { provider: "spotify", profile, accessToken, refreshToken };
-      return done(null, user);
+// ===== HANDLER FUNCTION =====
+async function handleUser(profile, accessToken, refreshToken, provider, done) {
+  try {
+    let email = null;
+    if (profile.emails && profile.emails.length > 0) {
+      email = profile.emails[0].value;
     }
-  )
-);
 
-// ===== GOOGLE =====
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      const user = { provider: "google", profile, accessToken, refreshToken };
-      return done(null, user);
+    let user = await User.findOne({ provider, providerId: profile.id });
+
+    if (!user) {
+      user = await User.create({
+        provider,
+        providerId: profile.id,
+        displayName: profile.displayName || profile.username,
+        email,
+        accessToken,
+        refreshToken,
+        profile,
+      });
+    } else {
+      // Update tokens on login
+      user.accessToken = accessToken;
+      user.refreshToken = refreshToken;
+      await user.save();
     }
-  )
-);
 
-// ===== FACEBOOK =====
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_APP_ID,
-      clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-      profileFields: ["id", "displayName", "emails"],
-    },
-    (accessToken, refreshToken, profile, done) => {
-      const user = { provider: "facebook", profile, accessToken, refreshToken };
-      return done(null, user);
-    }
-  )
-);
+    return done(null, user);
+  } catch (err) {
+    return done(err, null);
+  }
+}
 
-// ===== TWITTER =====
-passport.use(
-  new TwitterStrategy(
-    {
-      consumerKey: process.env.TWITTER_CONSUMER_KEY,
-      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: process.env.TWITTER_CALLBACK_URL,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      const user = { provider: "twitter", profile, accessToken, refreshToken };
-      return done(null, user);
-    }
-  )
-);
+// ===== STRATEGIES =====
+passport.use(new SpotifyStrategy({
+  clientID: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  callbackURL: process.env.SPOTIFY_CALLBACK_URL,
+}, (at, rt, exp, profile, done) => handleUser(profile, at, rt, "spotify", done)));
 
-// ===== SESSION HANDLING =====
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
+}, (at, rt, profile, done) => handleUser(profile, at, rt, "google", done)));
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+  profileFields: ["id", "displayName", "emails"],
+}, (at, rt, profile, done) => handleUser(profile, at, rt, "facebook", done)));
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: process.env.TWITTER_CALLBACK_URL,
+}, (at, rt, profile, done) => handleUser(profile, at, rt, "twitter", done)));
+
+// ===== SESSIONS =====
 passport.serializeUser((user, done) => {
-  done(null, user);
+  done(null, user._id);
 });
 
-passport.deserializeUser((obj, done) => {
-  done(null, obj);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 export default passport;
