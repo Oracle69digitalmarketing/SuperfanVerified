2d58a8ebb6f0b85505bee9fc0bc84798e465bc22
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-12 12:20:05 +0100

    Update passport.js
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-backend/config/passport.js
+++ superfan-backend/config/passport.js
@@ -1,6 +1,10 @@
 import passport from "passport";
 import SpotifyStrategy from "passport-spotify";
+import GoogleStrategy from "passport-google-oauth20";
+import FacebookStrategy from "passport-facebook";
+import TwitterStrategy from "passport-twitter";
 
+// ===== SPOTIFY =====
 passport.use(
   new SpotifyStrategy(
     {
@@ -8,24 +12,60 @@ passport.use(
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
       callbackURL: process.env.SPOTIFY_CALLBACK_URL,
     },
-    async (accessToken, refreshToken, expires_in, profile, done) => {
-      try {
-        // Save or update user in DB
-        const user = {
-          id: profile.id,
-          displayName: profile.displayName,
-          emails: profile.emails,
-          accessToken,
-          refreshToken,
-        };
-        return done(null, user);
-      } catch (err) {
-        return done(err, null);
-      }
+    (accessToken, refreshToken, expires_in, profile, done) => {
+      const user = { provider: "spotify", profile, accessToken, refreshToken };
+      return done(null, user);
     }
   )
 );
 
+// ===== GOOGLE =====
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const user = { provider: "google", profile, accessToken, refreshToken };
+      return done(null, user);
+    }
+  )
+);
+
+// ===== FACEBOOK =====
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      profileFields: ["id", "displayName", "emails"],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const user = { provider: "facebook", profile, accessToken, refreshToken };
+      return done(null, user);
+    }
+  )
+);
+
+// ===== TWITTER =====
+passport.use(
+  new TwitterStrategy(
+    {
+      consumerKey: process.env.TWITTER_CONSUMER_KEY,
+      consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+      callbackURL: process.env.TWITTER_CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const user = { provider: "twitter", profile, accessToken, refreshToken };
+      return done(null, user);
+    }
+  )
+);
+
+// ===== SESSION HANDLING =====
 passport.serializeUser((user, done) => {
   done(null, user);
 });
