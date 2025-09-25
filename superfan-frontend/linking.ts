5a5604ba8c1b59f569aaa3f8bb9f00bd2c959262
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-25 09:52:54 +0100

    Create linking.ts
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

---
+++ superfan-frontend/linking.ts
@@ -0,0 +1,25 @@
+// linking.ts
+
+const linking = {
+  prefixes: ["superfanverified://"],
+  config: {
+    screens: {
+      Home: "home",
+      Wallet: "wallet",
+      Dashboard: "dashboard",
+      ArtistDashboard: "artist",
+      QRScanner: "scan",
+      Scans: "scans",
+      Users: "users",
+      LeaderboardScreen: "leaderboard",
+      VotingHistory: "voting",
+      EventCheckIn: "event",
+      Staking: "staking",
+      Governance: "governance",
+      VerificationScreen: "verify",
+      ScoreScreen: "score",
+    },
+  },
+};
+
+export default linking;
