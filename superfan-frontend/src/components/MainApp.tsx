289a11f717bd8830a7f9e9ad39406590c4496e59
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-25 10:10:15 +0100

    Update MainApp.tsx
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-frontend/src/components/MainApp.tsx
+++ superfan-frontend/src/components/MainApp.tsx
@@ -1,7 +1,40 @@
 import React, { useEffect } from "react";
 import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import * as SQLite from "expo-sqlite";
+import * as SQLite fr// MainApp.tsx
+import React, { useEffect } from "react";
+import { LogBox } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as Sentry from "sentry-expo";
+
+import { AbstraxionProvider } from "@burnt-labs/abstraxion-react-native";
+import AppNavigator from "./AppNavigator";
+
+import linking from "./linking";
+import { setupDatabase } from "./db";
+import SessionBootstrap from "./SessionBootstrap";
+
+// --- Root App ---
+export default function MainApp() {
+  useEffect(() => {
+    setupDatabase();
+    LogBox.ignoreLogs(["Non-serializable values"]); // keep logs clean
+  }, []);
+
+  return (
+    <AbstraxionProvider
+      config={{
+        appName: "SuperfanVerified",
+        chainId: "xion-testnet-1", // swap for mainnet later
+      }}
+    >
+      <NavigationContainer linking={linking}>
+        <SessionBootstrap />
+        <AppNavigator />
+      </NavigationContainer>
+    </AbstraxionProvider>
+  );
+}om "expo-sqlite";
 import * as Sentry from "sentry-expo";
 import AppNavigator from "./AppNavigator";
 import {
