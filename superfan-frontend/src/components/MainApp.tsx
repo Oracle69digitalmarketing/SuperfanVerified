952457cc152107c442f9749362fa224b761687ed
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-25 18:50:11 +0100

    Update MainApp.tsx
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-frontend/src/components/MainApp.tsx
+++ superfan-frontend/src/components/MainApp.tsx
@@ -1,33 +1,28 @@
-// MainApp.tsx
 import React, { useEffect } from "react";
 import { LogBox } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import * as Sentry from "sentry-expo";
-
 import { AbstraxionProvider } from "@burnt-labs/abstraxion-react-native";
-import AppNavigator from "./AppNavigator";
 
+import RootNavigator from "./RootNavigator";
 import linking from "./linking";
 import { setupDatabase } from "./db";
-import SessionBootstrap from "./SessionBootstrap";
 
-// --- Root App ---
 export default function MainApp() {
   useEffect(() => {
     setupDatabase();
-    LogBox.ignoreLogs(["Non-serializable values"]); // keep logs clean
+    LogBox.ignoreLogs(["Non-serializable values"]);
   }, []);
 
   return (
     <AbstraxionProvider
       config={{
         appName: "SuperfanVerified",
-        chainId: "xion-testnet-1", // swap for mainnet later
+        chainId: "xion-testnet-1", // update for mainnet later
       }}
     >
       <NavigationContainer linking={linking}>
-        <SessionBootstrap />
-        <AppNavigator />
+        <RootNavigator />
       </NavigationContainer>
     </AbstraxionProvider>
   );
