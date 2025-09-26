2deaf0acc7ff96c924c9f43be6fddc3e6f4fac94
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-26 08:28:51 +0100

    Update App.tsx
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-frontend/src/App.tsx
+++ superfan-frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* Add more routes like /auth, /dashboard, etc. */}
+        <Route path="/auth" element={<Auth />} />
       </Routes>
     </div>
   );
