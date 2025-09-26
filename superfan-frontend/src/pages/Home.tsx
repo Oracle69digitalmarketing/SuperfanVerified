384c386d50bd949b453359bb2fef0eb5e8bef001
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-26 08:25:40 +0100

    Create Home.tsx
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

---
+++ superfan-frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl font-bold text-indigo-600">Superfan Verified</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        🚀 Frontend is live and Tailwind is working!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
