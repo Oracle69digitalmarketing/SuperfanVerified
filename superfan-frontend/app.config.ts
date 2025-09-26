4ca015ac5a9dc2ed57aec617dcf10d11e76e6d6a
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-26 09:33:40 +0100

    Update and rename app.config.js to app.config.ts
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-frontend/app.config.ts
+++ superfan-frontend/app.config.ts
@@ -31,25 +31,29 @@ const config: ExpoConfig = {
     favicon: './assets/favicon.png',
   },
   extra: {
-    // Xion / RUM
+    // --- Xion / RUM ---
     RUM_CONTRACT_ADDRESS: process.env.EXPO_PUBLIC_RUM_CONTRACT_ADDRESS,
     RECLAIM_PROVIDER_ID: process.env.EXPO_PUBLIC_RECLAIM_PROVIDER_ID,
 
-    // Spotify
+    // --- Spotify ---
     SPOTIFY_CLIENT_ID: process.env.EXPO_PUBLIC_SPOTIFY_CLIENT_ID,
     SPOTIFY_CALLBACK_URL: process.env.EXPO_PUBLIC_SPOTIFY_CALLBACK_URL,
 
-    // Google
+    // --- Google ---
     GOOGLE_CLIENT_ID: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
     GOOGLE_CALLBACK_URL: process.env.EXPO_PUBLIC_GOOGLE_CALLBACK_URL,
 
-    // Facebook
+    // --- Facebook ---
     FACEBOOK_APP_ID: process.env.EXPO_PUBLIC_FACEBOOK_APP_ID,
     FACEBOOK_CALLBACK_URL: process.env.EXPO_PUBLIC_FACEBOOK_CALLBACK_URL,
 
-    // Twitter
+    // --- Twitter ---
     TWITTER_CONSUMER_KEY: process.env.EXPO_PUBLIC_TWITTER_CONSUMER_KEY,
     TWITTER_CALLBACK_URL: process.env.EXPO_PUBLIC_TWITTER_CALLBACK_URL,
+
+    // --- LinkedIn ---
+    LINKEDIN_CLIENT_ID: process.env.EXPO_PUBLIC_LINKEDIN_CLIENT_ID,
+    LINKEDIN_CALLBACK_URL: process.env.EXPO_PUBLIC_LINKEDIN_CALLBACK_URL,
   },
 };
 
