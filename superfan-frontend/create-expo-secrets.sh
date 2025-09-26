e0f1257ec8ffec37da8cc8fbc423c2f8e69f555b
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-26 09:05:24 +0100

    Create create-expo-secrets.sh
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

---
+++ superfan-frontend/create-expo-secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ---- Spotify ----
+eas secret:create --name EXPO_PUBLIC_SPOTIFY_CLIENT_ID --value 46fff09224634943846be66c31394555
+
+# ---- Google ----
+eas secret:create --name EXPO_PUBLIC_GOOGLE_CLIENT_ID --value 172315192975-t9i7cormbb7ulmgcb99i3jdmrkgphc1g.apps.googleusercontent.com
+
+# ---- Facebook ----
+eas secret:create --name EXPO_PUBLIC_FACEBOOK_APP_ID --value 1364017365148309
+
+# ---- Twitter ----
+eas secret:create --name EXPO_PUBLIC_TWITTER_CONSUMER_KEY --value LuP7Hcre4Wn3xVfdxQmlUKk0N
+
+# ---- Reclaim Provider ----
+eas secret:create --name EXPO_PUBLIC_RECLAIM_PROVIDER_ID --value xxxxx
