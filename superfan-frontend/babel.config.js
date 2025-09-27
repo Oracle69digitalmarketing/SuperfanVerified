a9048c5e5f39bd40c377397caff2ac1041c04c68
Prince Adewale Adewumi <adewaleadewumi78@gmail.com>
2025-09-27 14:12:44 +0100

    Update babel.config.js
    Signed-off-by: Prince Adewale Adewumi <adewaleadewumi78@gmail.com>

--- superfan-frontend/babel.config.js
+++ superfan-frontend/babel.config.js
@@ -2,14 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
-      "babel-preset-expo", // Essential for Expo apps
-      "@babel/preset-env", // For web targets
+      "babel-preset-expo",          // Essential for Expo apps
+      "@babel/preset-env",          // For web targets
       ["@babel/preset-react", { runtime: "automatic" }], // React JSX
-      "@babel/preset-typescript" // TypeScript support
+      "@babel/preset-typescript"    // TypeScript support
     ],
     plugins: [
       "@babel/plugin-transform-runtime", // Reuse Babel helpers
-      "react-native-reanimated/plugin" // Only if using Reanimated
-    ].filter(Boolean), // Keeps the array clean if plugin conditional
+      "react-native-reanimated/plugin",  // Only if using Reanimated
+      ["@babel/plugin-transform-class-properties", { loose: true }],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-transform-private-property-in-object", { loose: true }]
+    ].filter(Boolean),
   };
 };
